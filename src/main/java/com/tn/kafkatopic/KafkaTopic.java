08c8d4dbecfe7258f616448eb2a05f5055c47e72
Nguyễn Văn Tuyên <v.tuyennv18@vinhomes.vn>
2022-10-13 10:45:01 +0700

    Update

--- src/main/java/com/tn/kafkatopic/KafkaTopic.java
+++ src/main/java/com/tn/kafkatopic/KafkaTopic.java
@@ -1,5 +1,7 @@
 package com.tn.kafkatopic;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class KafkaTopic {
 
     @KafkaListener(topics = "testTopic", groupId = "group-id")
-    public void kafkaTestTopic(String message) {
+    public void kafkaTestTopic(String message) throws JsonProcessingException {
         System.out.println("Message: " + message);
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        Student student = objectMapper.readValue(message, Student.class);
+//        System.out.println("student");
+//        System.out.println(student);
     }
 
 }
---
+++ src/main/java/com/tn/kafkatopic/Student.java
@@ -0,0 +1,12 @@
+package com.tn.kafkatopic;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private Integer id;
+
+    private String studentName;
+
+}
